c1dcd1f8cf3c73ef4095f29c6450828dd45e681d
Christian Nwamba <nwambachristian@gmail.com>
2016-02-23 14:53:47 +0100

    Sending emails without queues

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+use Mail;
+
+class HomeController extends Controller
+{
+
+    public function index()
+    {
+        return view()->make("home.index");
+    }
+
+
+    public function send()
+    {
+        Log::info("Email sending started");
+        Mail::send('email.welcome', ['data'=>'data'], function ($message) {
+
+            $message->from('nwambachristian@gmail.com', 'Christian Nwmaba');
+
+            $message->to('nwambachristian@gmail.com');
+
+        });
+        Log::info("Email sending finished");
+    }
+
+
+}
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,19 @@
+
+<html>
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <style>
+        .main{
+            margin: 100px auto;
+        }
+
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="main">
+        <a href="/send" class="btn btn-default">Send Email</a>
+    </div>
+</div>
+</body>
+</html>
